7433366f2a0e2d44ac81a10e760da3452c298a3e
Ammie Wang <ammie.wang8@gmail.com>
2021-01-08 02:41:24 -0800

    updated look!

--- changespeed.js
+++ changespeed.js
@@ -1,22 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('reset');
-  checkPageButton.addEventListener('click', function() {
+  var resetButton = document.getElementById('reset');
+  resetButton.addEventListener('click', function() {
     var inp = document.getElementsByTagName('input')[0];
     inp.value = 1;
     chrome.tabs.executeScript({code: "var vid = document.querySelectorAll('video')[0];  vid.playbackRate = 1;"});
   },false);
-  var checkPageButton = document.getElementById('plus');
-  checkPageButton.addEventListener('click', function() {
+  var plusButton = document.getElementById('plus');
+  plusButton.addEventListener('click', function() {
     var inp = document.getElementsByTagName('input')[0];
     inp.value = (parseFloat(inp.value)+.25);
     chrome.tabs.executeScript({code: "var vid = document.querySelectorAll('video')[0];  vid.playbackRate += .25;"});
   },false);
-  var checkPageButton = document.getElementById('minus');
-  checkPageButton.addEventListener('click', function() {
+  var minusButton = document.getElementById('minus');
+  minusButton.addEventListener('click', function() {
     var inp = document.getElementsByTagName('input')[0];
     var count = (parseFloat(inp.value)-.25);
     if (count < .25){ count = .25}
     else {chrome.tabs.executeScript({code: "var vid = document.querySelectorAll('video')[0];  vid.playbackRate -= .25;"})};
+    inp.value = count;
+  },false);
+  var speedButton = document.getElementById('speed');
+  speedButton.addEventListener('change', function() {
+    var inp = document.getElementsByTagName('input')[0];
+    var count = parseFloat(inp.value);
+    if (count < .25){
+      count = .25
+      chrome.tabs.executeScript({code: "var vid = document.querySelectorAll('video')[0];  vid.playbackRate = .25; "});
+    } else {
+      chrome.tabs.executeScript({code: "var vid = document.querySelectorAll('video')[0];  vid.playbackRate =" + inp.value + ";"});
+    }
     inp.value = count;
   },false);
 }, false);
